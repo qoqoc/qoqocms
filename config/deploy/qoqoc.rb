6e1ae9ff671bf620003dbf1fb11cca0b65e193af
vladdruzh <vlad.druzhinin@gmail.com>
2012-10-12 23:15:19 +0400

    production db, seed

--- config/deploy/qoqoc.rb
+++ config/deploy/qoqoc.rb
@@ -4,22 +4,26 @@ role :db,  "qoqoc.ru", :primary => true # This is where Rails migrations will ru
 
 set :deploy_to, "/home/www/projects/#{application}"
 
+set :passenger_port, "3001"
+set :passenger_pid, "#{shared_path}/tmp/pids/passenger.#{passenger_port}.pid"
+
 namespace :deploy do
 
   desc "Start application"
   task :start, :roles => :app do
+    run "cd #{latest_release} && bundle exec nohup RAILS_ENV=#{rails_env} passenger start -p #{passenger_port} &"
   end
 
   desc "Stop application"
   task :stop, :roles => :app do
+    run "[ -f #{passenger_pid} ] && kill -QUIT `cat #{passenger_pid}`"
   end
 
   desc "Restart Application"
   task :restart, :roles => :app do
-  end
-
-  desc "Restart Application"
-  task :reload, :roles => :app do
+    stop
+    sleep 5
+    start
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 User.find_or_create_by_email('admin@qoqoc.ru') do  |u|
   u.password = 'adminqoqocik'
 end
+User.find_or_create_by_email('stans20@mail.ru') do  |u|
+  u.password = 'stansadm20'
+end
